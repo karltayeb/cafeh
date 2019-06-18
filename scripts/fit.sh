a20cd109db51c938c02694ea9080ac71a0f7e6fd
Karl Tayeb <karl.tayeb7@gmail.com>
2019-06-18 15:31:08 -0400

    slrum script

---
+++ scripts/fit.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH -p unlimited
+#SBATCH -t 1:0:0
+
+module load singularity
+
+# redefine SINGULARITY_HOME to mount current working directory to base $HOME
+
+GENE=$(ls /work-zfs/abattle4/karl/correlation_matrices/ | awk -v var=$SLURM_ARRAY_TASK_ID 'NR==var')
+
+cd /working/abattle4/karl/
+export SINGULARITY_HOME=$PWD:/home/$USER
+singularity exec shub://karltayeb/gp_fine_mapping python fit.py $GENE
