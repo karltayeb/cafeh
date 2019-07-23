ce19bd5814c7af67bf5a1c1b75acc4c05c4f7872
Karl Tayeb <karl.tayeb7@gmail.com>
2019-07-23 11:15:34 -0400

    added more arguments

--- sim_from_snps.py
+++ sim_from_snps.py
@@ -5,14 +5,15 @@ from training_and_plotting import train
 import autograd.numpy as np
 import autograd.scipy as sp
 import pandas as pd
+import sys
 import pickle
 
-M, T, = 1, 10
+M, T, function, transform, train_params = int(sys.argv[1]), int(sys.argv[2]), sys.argv[3], sys.argv[4], bool(int(sys.argv[5]))
 
 GENE_PATH = './top_genes/'
 CORRELATION_PATH = './data/correlations/'
 MODEL_SAVE_PATH = './output/'
-ITERS = 10000
+ITERS = 1000
 
 names = ['tissue', 'variant_id', 'tss_distance', 'ma_samples', 'ma_count', 'maf', 'pval_nominal', 'slope', 'slope_se']
 gene = 'ENSG00000101307.15'
@@ -36,12 +37,18 @@ Sigma = r_df.values[in_range][:, in_range]
 u, s, vh = np.linalg.svd(Sigma)
 X = (u * np.sqrt(s))[:, :900]
 
-def generate_data(Sigma, num_causal=1, effect_size=1, num_tissues=1):
+ldness = np.mean(np.sort(np.abs(Sigma), 1)[:, -11:-1], 1)
+causal_snps = np.unique([np.abs(ldness - target_ld).argmin() for target_ld in np.linspace(0, 1, 11)])
+
+def generate_data(Sigma, causal_snps=None, effect_size=1, num_tissues=1):
     N = Sigma.shape[0]
     causal_status = np.zeros(N)
-    causal_status[np.random.choice(N, num_causal)] = effect_size
+
+    if causal_snps is None:
+        causal_snps = np.random.choice(N, 1)
+    causal_status[causal_snps] = effect_size
     expected_effects = Sigma @ causal_status[:, None]
-    
+
     L = np.linalg.cholesky(Sigma + np.eye(N) * 1e-6)
     observed_effects = L @ np.random.normal(size=(N, num_tissues)) + expected_effects
     return causal_status, expected_effects, observed_effects
@@ -50,45 +57,46 @@ data_dict = {}
 save_dict = {}
 marginal_functions = [x for x in dir(marginal_models) if 'functions' in x]
 
-for run in range(3):
-    causal, expected, Y = generate_data(Sigma, num_causal=1, effect_size=1, num_tissues=T)
-    data_dict[run] = (causal, expected, Y)
 
-    for function in marginal_functions:
+fname = '_'.join(function.split('_')[:-1])
+tname = '_'.join(transform.split('_')[:-1])
+
+funcs = getattr(marginal_models, function)
+trans = getattr(autograd_transforms, transform)
+
+
+for run in range(2):
+    data_dict[run] = {}
+    for causal_snp in causal_snps:
+        causal, expected, Y = generate_data(Sigma, causal_snps=causal_snp, effect_size=1, num_tissues=T)
+        data_dict[run][causal_snp] = (causal, expected, Y)
         print(function)
-        for transform in ['normalize_transform']:
-            if 'independent' in function:
-                for kwargs in [{'train_A': False}, {'train_A': True}]:
-                    try:
-                        fname = '_'.join(function.split('_')[:-1])
-                        tname = '_'.join(transform.split('_')[:-1])
-                        key = '{}_{}_{}_{}_{}_{}'.format(fname, tname, M, T, kwargs['train_A'], run)
+        if 'independent' in function:
+            kwargs = {'train_A': train_params}
+            key = '{}_{}_{}_{}_{}_{}_{}'.format(fname, tname, M, T, kwargs['train_A'], causal_snp, run)
+            print('\t{}'.format(key))
+            Z, Z_init, params = train(funcs, X, Y, Sigma, M=M, transform=trans, kwargs=kwargs, verbose=False)
+            save_dict[key] = (Z, Z_init, params)
+        else:
+            try:
+                key = '{}_{}_{}_{}_{}_{}_{}'.format(fname, tname, M, T, False, causal_snp, run)
+                print('\t{}'.format(key))
+                Z, Z_init, params = train(funcs, X, Y, Sigma, M=M, transform=trans, verbose=False)
+                save_dict[key] = (Z, Z_init, params)
+            except:
+                print('\t!!!!!!!')
 
-                        print('\t{}'.format(key))
-                        funcs = getattr(marginal_models, function)
-                        trans = getattr(autograd_transforms, transform)
-                        Z, Z_init, params = train(funcs, X, Y, Sigma, M=M, transform=trans, kwargs=kwargs, verbose=False)
-                        save_dict[key] = (Z, Z_init, params)
-                    except:
-                        print('\t!!!!!!!')
-            else:
-                try:
-                    fname = '_'.join(function.split('_')[:-1])
-                    tname = '_'.join(transform.split('_')[:-1])
-                    key = '{}_{}_{}_{}_{}_{}'.format(fname, tname, M, T, False, run)
+    if (run % 10) == 0:
+        # save save_dict
+        save_name = '{}_M{}_T{}_{}_{}_{}_ouputs'.format(gene, M, T, fname, tname, train_params)
+        pickle.dump(save_dict, open(save_name, 'wb'))
 
-
-                    print('\t{}'.format(key))
-                    funcs = getattr(marginal_models, function)
-                    trans = getattr(autograd_transforms, transform)
-                    Z, Z_init, params = train(funcs, X, Y, Sigma, M=M, transform=trans, verbose=False)
-                    save_dict[key] = (Z, Z_init, params)
-                except:
-                    print('\t!!!!!!!')
+        data_save_name = '{}_M{}_T{}_{}_{}_{}_data'.format(gene, M, T, fname, tname, train_params)
+        pickle.dump(data_dict, open(data_save_name, 'wb'))
 
 # save save_dict
-save_name = '{}_M{}_T{}_ouputs'.format(gene, M, T)
+save_name = '{}_M{}_T{}_{}_{}_{}_ouputs'.format(gene, M, T, fname, tname, train_params)
 pickle.dump(save_dict, open(save_name, 'wb'))
 
-data_save_name = '{}_M{}_T{}_data'.format(gene, M, T)
+data_save_name = '{}_M{}_T{}_{}_{}_{}_data'.format(gene, M, T, fname, tname, train_params)
 pickle.dump(data_dict, open(data_save_name, 'wb'))
