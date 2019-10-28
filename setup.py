69c4a57f953395102f48369143939645267355fe
Karl Tayeb <karl.tayeb7@gmail.com>
2019-10-28 11:03:44 -0400

    added setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='coloc',
+      version='1.0',
+      description='Python Distribution Utilities',
+      author='Karl Tayeb',
+      packages=['coloc'],
+     )
