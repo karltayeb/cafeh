584e93da8f1acce999acc45b21a9aedebade22ee
Karl Tayeb <karl.tayeb7@gmail.com>
2020-04-07 09:14:44 -0400

    b

--- coloc/independent_model2.py
+++ coloc/independent_model2.py
@@ -314,8 +314,9 @@ class IndependentFactorSER:
         # <ln p (w | alpha) > - <ln q(w)> - <ln q(w)>
         Elna = digamma(self.ard_precision_a) - np.log(self.ard_precision_b)  # [T, K]
         precision = self.expected_weight_precision()  # [T, K]
-        w2 = ((self.weight_means**2 + self.weight_vars) * self.pi[None]).sum(2)  # [T, K, N]
-        entropy = (normal_entropy(self.weight_vars) * self.pi[None]).sum(2)  # [T, K, N]
+        w2 = ((self.weight_means**2 + self.weight_vars) * self.pi[None]).sum(-1)  # [T, K]
+        entropy = (normal_entropy(self.weight_vars) * self.pi[None]).sum(-1)  # [T, K]
+        import pdb; pdb.set_trace()
         KL += np.sum(
             (precision / 2) * w2 - entropy
         )
