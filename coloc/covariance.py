65dfaada53ddd0638651d93370d7b870956e07f6
Karl Tayeb <karl.tayeb7@gmail.com>
2020-06-12 17:19:09 -0400

    covariance functions

---
+++ coloc/covariance.py
@@ -0,0 +1,17 @@
+import numpy as np
+from sklearn import covariance
+
+def cov2corr(X):
+    """
+    scale covariance matrix to correlaton matrix
+    """
+    diag = np.sqrt(np.diag(X))
+    return (1/diag[:, None]) * X * (1/diag[None])
+
+sample_ld = lambda data: np.corrcoef(data.X.T)
+refernce_ld = lambda data: np.corrcoef(data.X1kG.T)
+z_ld = lambda data: np.corrcoef(data.B.T.values / np.sqrt(data.V.T.values))
+ledoit_wolf_sample_ld = lambda data: cov2corr(covariance.ledoit_wolf(data.X)[0])
+ledoit_wolf_reference_ld = lambda data: cov2corr(covariance.ledoit_wolf(data.X1kG)[0])
+ledoit_wolf_z_ld = lambda data: cov2corr(covariance.ledoit_wolf(data.B.values / np.sqrt(data.V.values))[0])
+z3_ld = lambda data: z_ld(data)**3
