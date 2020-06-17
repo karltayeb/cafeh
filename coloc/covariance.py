1214c1f590e50495e7720f57cc5f8b8e6d3f83e6
Karl Tayeb <karl.tayeb7@gmail.com>
2020-06-17 12:33:21 -0400

    fix 1kg populations

--- coloc/covariance.py
+++ coloc/covariance.py
@@ -3,7 +3,9 @@ from sklearn import covariance
 import pickle
 from .misc import center_mean_impute
 
-superpop2samples = pickle.load('/work-zfs/abattle4/karl/cosie_analysis/output/superpop2samples_1kG')
+superpop2samples = pickle.load(open(
+    '/work-zfs/abattle4/karl/cosie_analysis/output/superpop2samples_1kG', 'rb'))
+
 def cov2corr(X):
     """
     scale covariance matrix to correlaton matrix
