c33282b300a6a82d38192104a1b8f0a5170d16f1
karltayeb <karl.tayeb7@gmail.com>
2021-06-15 18:40:09 -0400

    reorganize/clean out plotting function

--- cafeh/plotting.py
+++ cafeh/plotting.py
@@ -9,64 +9,6 @@ import pickle
 # PLOTTING FUNCTIONS #
 ######################
 
-def plot_assignment_kl(self, thresh=0.5, save_path=None, show=True):
-    """
-    Plot similarity between causal components
-    If CAFEH is run without estimating effect sizes, and prior_variance is too small
-    you will see redundancy in the components.
-    """
-    kls = np.zeros((self.dims['K'], self.dims['K']))
-    for k1 in range(self.dims['K']):
-        for k2 in range(self.dims['K']):
-            kls[k1, k2] = categorical_kl(self.pi.T[:, k1], self.pi.T[:, k2])
-    active = np.any(self.active > thresh, 0)
-    sns.heatmap(kls[active][:, active], cmap='Blues', xticklabels=np.arange(self.dims['K'])[active], yticklabels=np.arange(self.dims['K'])[active])
-    plt.title('Pairwise KL of Component Multinomials')
-    plt.xlabel('Component')
-    plt.ylabel('Component')
-    if save_path is not None:
-        plt.savefig(save_path)
-    if show:
-        plt.show()
-    plt.close()
-
-
-def plot_credible_sets_ld(self, snps=None, alpha=0.9, thresh=0.5, save_path=None, show=True):
-    """
-    Heatmap of LD among `alpha` credible sets with `p_active > thresh` in atleast one study
-    can also include additional snps via snps argument
-    """
-    if snps is None:
-        snps = []
-    credible_sets, purity = self.get_credible_sets(alpha=alpha)
-    for k in np.arange(self.dims['K']):
-        if purity[k] > thresh:
-            snps.append(credible_sets[k])
-
-    sizes = np.array([x.size for x in snps])
-    snps = np.concatenate(snps)
-
-    fig, ax = plt.subplots(1, figsize=(6, 5))
-    ld = self.get_ld(snps=snps)
-    if np.ndim(ld) == 2:
-        ld = ld ** 2
-    else:
-        ld = (ld ** 2).min(0)
-    sns.heatmap(
-        ld, cmap='RdBu_r', center=0, ax=ax,
-        square=True, annot=False, cbar=True,
-        yticklabels=self.snp_ids[snps], xticklabels=[]
-    )
-    ax.hlines(np.cumsum(sizes), *ax.get_xlim(), colors='w', lw=3)
-    ax.vlines(np.cumsum(sizes), *ax.get_ylim(), colors='w', lw=3)
-    plt.title('alpha={} confidence set LD'.format(alpha))
-    if save_path is not None:
-        plt.savefig(save_path)
-    if show:
-        plt.show()
-    plt.close()
-    
-
 def plot_components(self, thresh=0.5, save_path=None, show=True):
     """
     plot inducing point posteriors, weight means, and probabilities
@@ -104,6 +46,67 @@ def plot_components(self, thresh=0.5, save_path=None, show=True):
     ax[0].set_title('active')
     ax[0].set_xlabel('component')
 
+    if save_path is not None:
+        plt.savefig(save_path)
+    if show:
+        plt.show()
+    plt.close()
+
+######
+# Experimental/not supported plots
+######
+
+def plot_assignment_kl(self, thresh=0.5, save_path=None, show=True):
+    """
+    Plot similarity between causal components
+    If CAFEH is run without estimating effect sizes, and prior_variance is too small
+    you will see redundancy in the components.
+    """
+    kls = np.zeros((self.dims['K'], self.dims['K']))
+    for k1 in range(self.dims['K']):
+        for k2 in range(self.dims['K']):
+            kls[k1, k2] = categorical_kl(self.pi.T[:, k1], self.pi.T[:, k2])
+    active = np.any(self.active > thresh, 0)
+    sns.heatmap(kls[active][:, active], cmap='Blues', xticklabels=np.arange(self.dims['K'])[active], yticklabels=np.arange(self.dims['K'])[active])
+    plt.title('Pairwise KL of Component Multinomials')
+    plt.xlabel('Component')
+    plt.ylabel('Component')
+    if save_path is not None:
+        plt.savefig(save_path)
+    if show:
+        plt.show()
+    plt.close()
+
+
+def plot_credible_sets_ld(self, snps=None, alpha=0.9, thresh=0.5, save_path=None, show=True):
+    """
+    Heatmap of LD among `alpha` credible sets
+    provides visual indication of linkage between multiple causal components
+    """
+    if snps is None:
+        snps = []
+    credible_sets, purity = self.get_credible_sets(alpha=alpha)
+    for k in np.arange(self.dims['K']):
+        if purity[k] > thresh:
+            snps.append(credible_sets[k])
+
+    sizes = np.array([x.size for x in snps])
+    snps = np.concatenate(snps)
+
+    fig, ax = plt.subplots(1, figsize=(6, 5))
+    ld = self.get_ld(snps=snps)
+    if np.ndim(ld) == 2:
+        ld = ld ** 2
+    else:
+        ld = (ld ** 2).min(0)
+    sns.heatmap(
+        ld, cmap='RdBu_r', center=0, ax=ax,
+        square=True, annot=False, cbar=True,
+        yticklabels=self.snp_ids[snps], xticklabels=[]
+    )
+    ax.hlines(np.cumsum(sizes), *ax.get_xlim(), colors='w', lw=3)
+    ax.vlines(np.cumsum(sizes), *ax.get_ylim(), colors='w', lw=3)
+    plt.title('alpha={} confidence set LD'.format(alpha))
     if save_path is not None:
         plt.savefig(save_path)
     if show:
